f8c35d1b2513f8aab08464e129819f78dd5f3151
danialcala94 <danielalcalavalera@gmail.com>
2026-07-06 03:46:41 +0700

    Removed 'local_dir_use_symlinks' as it has been deprecated. New version.

--- src/aibusy_runtime_huggingface/service/huggingface_client.py
+++ src/aibusy_runtime_huggingface/service/huggingface_client.py
@@ -19,8 +19,7 @@ class DefaultHuggingfaceClient(
         path = snapshot_download(
             repo_id = repository,
             revision = revision,
-            local_dir = local_dir,
-            local_dir_use_symlinks = False
+            local_dir = local_dir
         )
 
         return Path(path)
